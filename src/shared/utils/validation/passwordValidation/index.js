e664074e575c73353cb7bc71b15c28725514c54c
Gustavonn07 <gustavonepnog@gmail.com>
2025-01-15 21:48:15 -0300

    fix: validation password

--- src/shared/utils/validation/passwordValidation/index.js
+++ src/shared/utils/validation/passwordValidation/index.js
@@ -4,23 +4,26 @@ async function validatePassword(password) {
     const schema = yup.object().shape({
         password: yup
             .string()
-            .required('A senha é obrigatória')
-            .min(8, 'A senha deve ter no mínimo 8 caracteres')
+            .required('A senha é obrigatória.')
+            .matches(/[A-Z]/, 'A senha deve conter ao menos uma letra maiúscula.')
+            .matches(/[a-z]/, 'A senha deve conter ao menos uma letra minúscula.')
+            .matches(/[0-9]/, 'A senha deve conter ao menos um número.')
             .matches(
-                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                'A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial'
-            ),
+                /[!@#$%^&*(),.?":{}|<>]/,
+                'A senha deve conter ao menos um caractere especial (!@#$%^&*(),.?":{}|<>).'
+            )
+            .min(8, 'A senha deve ter pelo menos 8 caracteres.'),
     });
 
     try {
         await schema.validate({ password });
         return { valid: true };
-        
     } catch (error) {
         return {
             valid: false,
-            error: 'Erro na validação de senha: ' + error.message,
-            suggestion: 'Certifique-se de que a senha tenha pelo menos 8 caracteres, inclua uma letra maiúscula, uma letra minúscula, um número e um caractere especial como @$!%*?&',
+            error: 'Erro na validação de senha: ' + error.errors[0],
+            suggestion:
+                'Certifique-se de que a senha tenha pelo menos 8 caracteres, inclua uma letra maiúscula, uma letra minúscula, um número e um caractere especial como !@#$%^&*(),.?":{}|<>.',
         };
     }
 }
